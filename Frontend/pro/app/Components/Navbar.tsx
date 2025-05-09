78d16cf31e0877ef14866c64820cae3fdf21737b
SuperexMack <unilexerox@gmail.com>
2025-05-09 20:22:34 +0530

    Add link

--- Frontend/pro/app/Components/Navbar.tsx
+++ Frontend/pro/app/Components/Navbar.tsx
@@ -14,7 +14,7 @@ export function Navbar({bgvalue,setvalue}:any){
 
             {/* Name tag */}
            <div className="absolute md:left-[20%] left-[10%]">
-            <Link href={"/"} className={`${bgvalue%2==0 ? "text-white" : "text-black"} font-bold md:text-[30px] text-[18px]`}>TLE-Ele-App</Link>
+            <Link href={"/"} className={`${bgvalue%2==0 ? "text-white" : "text-black"} font-bold cursor-pointer md:text-[30px] text-[18px]`}>TLE-Ele-App</Link>
            </div>
 
            {/* Go Tag */}
